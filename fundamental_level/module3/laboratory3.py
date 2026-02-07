9c9247f0a71d9c25331ba7dd2836eb41fecc40ec
CeciliaGutierrez <ceciliagutierrezurbano@gmail.com>
2026-02-07 01:29:54 -0600

    feat: complete laboratory 3 implemented required exercises and validations

---
+++ fundamental_level/module3/laboratory3.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+from typing import List
+
+from pydantic import BaseModel
+
+
+# -------Dataclasses---------
+@dataclass
+class OrderItem:
+    name: str
+    price: float
+    quantity: int
+
+    def total(self) -> float:
+        return self.price * self.quantity
+
+
+@dataclass
+class Order:
+    items: List[OrderItem]
+
+    def total_price(self) -> float:
+        return sum(item.total() for item in self.items)
+
+
+# -----------input/out---------
+class OrderItemIn(BaseModel):
+    name: str
+    price: float
+    quantity: int
+
+
+class OrderIn(BaseModel):
+    items: List[OrderItemIn]
+
+
+class OrderOut(BaseModel):
+    total: float
+
+
+# ------Conversion logic-----
+
+
+def convert_to_order(order_in: OrderIn) -> Order:
+    items = [
+        OrderItem(
+            name=item.name,
+            price=item.price,
+            quantity=item.quantity,
+        )
+        for item in order_in.items
+    ]
+    return Order(items=items)
+
+
+if __name__ == "__main__":
+    data = {
+        "items": [
+            {"name": "Laptop", "price": 12000, "quantity": 1},
+            {"name": "Keyboard", "price": 600, "quantity": 2},
+        ]
+    }
+
+    order_in = OrderIn(**data)
+    order = convert_to_order(order_in)
+    total = order.total_price()
+    order_out = OrderOut(total=total)
+
+    print("Orden total:", order_out.total)
